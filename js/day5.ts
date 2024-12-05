f04b8534572146031977b8769fd1c0a7eb6d58a0
Gunnar Abrahamsson <gunnar.abrahamsson@ourliving.se>
2024-12-05 16:51:06 +0100

    day 5 refactor

--- js/day5.ts
+++ js/day5.ts
@@ -1,94 +1,57 @@
 const text = await Deno.readTextFile("./input_5.txt");
 
-const testText = `47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
+const lines = text.split("\n");
+const splitIndex = lines.indexOf("");
+const ruleLines = lines.slice(0, splitIndex);
+const pageLines = lines.slice(splitIndex + 1);
+const rules = new Set<string>();
 
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47`;
+ruleLines.forEach((rule) => rules.add(rule));
 
-const star1 = (text: string) => {
-  const lines = text.split("\n");
-  const splitIndex = lines.indexOf("");
-  const ruleLines = lines.slice(0, splitIndex);
-  const pageLines = lines.slice(splitIndex + 1);
-
+const star1 = () => {
   let answer = 0;
-
   pageLines.forEach((line) => {
-    const pages = line.split(",").map((page) => +page);
+    const pages = line.split(",").map((page) => page);
     const pagesSortedByRules = [...pages].sort((a, b) => {
-      const matchedABeforeB = ruleLines.filter((rule) => rule === `${a}|${b}`);
-      const matchedBBeforeA = ruleLines.filter((rule) => rule === `${b}|${a}`);
-      if (matchedABeforeB.length) {
+      if (rules.has(`${a}|${b}`)) {
         return -1;
       }
-      if (matchedBBeforeA.length) {
+      if (rules.has(`${b}|${a}`)) {
         return 1;
       }
       return 0;
     });
 
     if (pages.join(",") === pagesSortedByRules.join(",")) {
-      answer = answer + pages[Math.floor(pages.length / 2)];
+      answer = answer + +pagesSortedByRules[Math.floor(pages.length / 2)];
     }
   });
 
   return answer;
 };
 
-console.log(star1(text));
+console.log(star1());
 
-const star2 = (text: string) => {
-  const lines = text.split("\n");
-  const splitIndex = lines.indexOf("");
-  const ruleLines = lines.slice(0, splitIndex);
-  const pageLines = lines.slice(splitIndex + 1);
-
+const star2 = () => {
   let answer = 0;
-
   pageLines.forEach((line) => {
-    const pages = line.split(",").map((page) => +page);
+    const pages = line.split(",").map((page) => page);
     const pagesSortedByRules = [...pages].sort((a, b) => {
-      const matchedABeforeB = ruleLines.filter((rule) => rule === `${a}|${b}`);
-      const matchedBBeforeA = ruleLines.filter((rule) => rule === `${b}|${a}`);
-      if (matchedABeforeB.length) {
+      if (rules.has(`${a}|${b}`)) {
         return -1;
       }
-      if (matchedBBeforeA.length) {
+      if (rules.has(`${b}|${a}`)) {
         return 1;
       }
       return 0;
     });
 
     if (pages.join(",") !== pagesSortedByRules.join(",")) {
-      answer = answer + pagesSortedByRules[Math.floor(pages.length / 2)];
+      answer = answer + +pagesSortedByRules[Math.floor(pages.length / 2)];
     }
   });
 
   return answer;
 };
 
-console.log(star2(text));
+console.log(star2());
